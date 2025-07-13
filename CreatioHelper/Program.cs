409a98935db8d4b824774f79676449f23acac77b
fejdraus <46014577+fejdraus@users.noreply.github.com>
2025-07-13 16:23:18 +0300

    Update startup comments

--- CreatioHelper/Program.cs
+++ CreatioHelper/Program.cs
@@ -7,8 +7,8 @@ namespace CreatioHelper;
 class Program
 {
     // Initialization code. Don't use any Avalonia, third-party APIs or any
-    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
-    // yet and stuff might break.
+    // SynchronizationContext-reliant code before BuildAvaloniaApp is called:
+    // things aren't initialized yet and stuff might break.
     [STAThread]
     public static void Main(string[] args)
     {
