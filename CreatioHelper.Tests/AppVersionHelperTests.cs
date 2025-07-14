f6692b06320ec91f4d1529658e8594ee3292ee79
fejdraus <46014577+fejdraus@users.noreply.github.com>
2025-07-14 16:27:31 +0300

    Add core library unit tests

---
+++ CreatioHelper.Tests/AppVersionHelperTests.cs
@@ -0,0 +1,46 @@
+using System;
+using System.IO;
+using System.Reflection;
+using CreatioHelper.Core;
+
+namespace CreatioHelper.Tests;
+
+public class AppVersionHelperTests
+{
+    [Fact]
+    public void GetAppVersion_WhenFileMissing_ReturnsDefaultVersion()
+    {
+        var tempDir = Directory.CreateTempSubdirectory();
+        try
+        {
+            var version = AppVersionHelper.GetAppVersion(tempDir.FullName);
+            Assert.Equal(new Version(), version);
+        }
+        finally
+        {
+            tempDir.Delete(true);
+        }
+    }
+
+    [Fact]
+    public void GetAppVersion_WhenFileExists_ReturnsAssemblyVersion()
+    {
+        var tempDir = Directory.CreateTempSubdirectory();
+        try
+        {
+            var binDir = Path.Combine(tempDir.FullName, "bin");
+            Directory.CreateDirectory(binDir);
+            var sourceAssembly = Assembly.GetExecutingAssembly().Location;
+            var destAssembly = Path.Combine(binDir, "Terrasoft.Common.dll");
+            File.Copy(sourceAssembly, destAssembly);
+
+            var expected = AssemblyName.GetAssemblyName(sourceAssembly).Version;
+            var actual = AppVersionHelper.GetAppVersion(tempDir.FullName);
+            Assert.Equal(expected, actual);
+        }
+        finally
+        {
+            tempDir.Delete(true);
+        }
+    }
+}
