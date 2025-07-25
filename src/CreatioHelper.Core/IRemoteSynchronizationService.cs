d3373c659e211131dc48c1e5939d64fa4bc01df2
fejdraus <46014577+fejdraus@users.noreply.github.com>
2025-07-26 02:43:58 +0300

    Move settings to Domain/Infrastructure

--- src/CreatioHelper.Core/IRemoteSynchronizationService.cs
+++ src/CreatioHelper.Core/IRemoteSynchronizationService.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using CreatioHelper.Domain.Entities;
 using System.Threading;
 using System.Threading.Tasks;
 
---
+++ src/CreatioHelper.Domain/Entities/AppSettings.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.ObjectModel;
+
+namespace CreatioHelper.Domain.Entities;
+
+public class AppSettings
+{
+    public string? SitePath { get; set; }
+
+    public string? ServiceName { get; set; }
+
+    public string? SelectedIisSiteName { get; set; }
+
+    public string? PackagesPath { get; set; }
+
+    public string? PackagesToDeleteBefore { get; set; }
+
+    public string? PackagesToDeleteAfter { get; set; }
+
+    public ObservableCollection<ServerInfo> ServerList { get; set; } = new();
+
+    public bool IsIisMode { get; set; }
+
+    public bool IsServerPanelVisible { get; set; }
+}
