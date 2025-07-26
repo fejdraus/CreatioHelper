6adb05775f6ea4c40760bb1f241d869586123872
fejdraus <46014577+fejdraus@users.noreply.github.com>
2025-07-26 13:20:25 +0300

    Register output writer

--- src/CreatioHelper.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ src/CreatioHelper.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -1,5 +1,6 @@
 using CreatioHelper.Application.Interfaces;
 using CreatioHelper.Infrastructure.Services;
+using CreatioHelper.Core;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CreatioHelper.Infrastructure.Extensions;
@@ -8,6 +9,7 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services)
     {
+        services.AddSingleton<IOutputWriter, ConsoleOutputWriter>();
         services.AddSingleton<ISettingsService, SettingsService>();
         services.AddTransient<ISystemServiceManager, SystemServiceManager>();
         return services;
